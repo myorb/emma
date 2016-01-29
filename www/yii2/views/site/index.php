4ef56acc85c4d55621b696e10c43b68887b9f454
alex <itdep24@gmail.com>
2016-01-29 18:36:06 +0200

    change angular client

--- www/yii2/views/site/index.php
+++ www/yii2/views/site/index.php
@@ -6,7 +6,17 @@ $this->title = 'My Yii Application';
 ?>
 <div ng-app="myApp" ng-controller="todoCtrl">
 
-    <h2>My Todo List</h2>
+    <div ng-controller="ClickToEditCtrl">
+        <div ng-hide="editorEnabled">
+            <h2> {{title}}</h2><a href="#" ng-click="editorEnabled=!editorEnabled">Edit title</a>
+        </div>
+        <div ng-show="editorEnabled">
+            <h2><input ng-model="title"></h2>
+            <a href="#" ng-click="editorEnabled=!editorEnabled">Done editing?</a>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <hr>
 
     <form ng-submit="todoAdd()">
         <input type="text" ng-model="name" size="25" placeholder="Name">
@@ -14,7 +24,6 @@ $this->title = 'My Yii Application';
         <input type="submit" value="Add New">
     </form>
 
-    <br>
 
     <div ng-repeat="x in todoList">
         <input type="checkbox" ng-model="x.done"> Who:<b ng-bind="x.name"></b> Task: <b ng-bind="x.todoText"></b>
@@ -23,38 +32,58 @@ $this->title = 'My Yii Application';
     <p><button ng-click="remove()">Remove marked</button></p>
     <script>
         var app = angular.module('myApp', ['ngResource']);
+
         app.factory("Item", function($resource) {
             return $resource("/api/todo/:id");
         });
 
         app.controller('todoCtrl', function($scope,Item) {
 
-            Item.query(function(data) {
-                $scope.todoList = data;
-            });
+            $scope.todoList = Item.query();
 
             $scope.todoAdd = function() {
                 $scope.item = new Item();
+
                 $scope.item.todoText =$scope.todoInput;
                 $scope.item.name =$scope.name;
                 $scope.item.done = 0;
+
                 Item.save($scope.item);
-                $scope.todoList.push($scope.item);
+                $scope.todoList = Item.query();
+
                 $scope.todoInput = "";
             };
 
             $scope.remove = function() {
-                var oldList = $scope.todoList;
-                $scope.todoList = [];
-                angular.forEach(oldList, function(x) {
-                    if (x.done == 0){
-                        $scope.todoList.push(x);
-                    }else{
+                angular.forEach($scope.todoList, function(x) {
+                    if (x.done == 1){
                         Item.delete({id: x.id});
                     }
-
                 });
+                $scope.todoList = Item.query();
+            };
+
+        });
+
+        app.controller('ClickToEditCtrl', function($scope) {
+            $scope.title = "Welcome to this demo!";
+            $scope.editorEnabled = false;
+
+            $scope.enableEditor = function() {
+                $scope.editorEnabled = true;
+                $scope.editableTitle = $scope.title;
+            };
+
+            $scope.disableEditor = function() {
+                $scope.editorEnabled = false;
+            };
+
+            $scope.save = function() {
+                $scope.title = $scope.editableTitle;
+                $scope.disableEditor();
             };
         });
+
+
     </script>
 </div>
